25d09c2675798781ae77833cf82bd6c9f592bd91
rhnrafif <raihanudinrafif@gmail.com>
2022-09-17 00:44:32 +0700

    add dark mode cuyy

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,6 +15,41 @@ const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
+    hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden');
 });
+
+//klik area window
+window.addEventListener('click', (e)=>{
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+})
+
+//add btn dark
+const btn_dark = document.getElementById('btn_dark');
+const html = document.getElementById('html')
+let sun = document.getElementById('sun');
+let moon = document.getElementById('moon');
+
+btn_dark.addEventListener('click',()=>{
+
+    if(sun.classList.contains('scale-0')){
+        sun.classList.remove('scale-0');
+        sun.classList.add('scale-100');
+        moon.classList.remove('scale-100');
+        moon.classList.add('scale-0');
+            
+        html.classList.remove('dark')
+        return
+    }
+    moon.classList.remove('scale-0');
+    moon.classList.add('scale-100');
+    sun.classList.remove('scale-100');
+    sun.classList.add('scale-0');
+
+    html.classList.add('dark')
+
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode : 'class',
   theme: {
     container:{
       center: true,
@@ -12,6 +13,7 @@ module.exports = {
         primary: '#00A7EB',
         dark: '#0f172a',
         secondary: '#254A5C',
+        third: '#d9d9d9'
       },
       screens:{
         '2xl':'1320px',
